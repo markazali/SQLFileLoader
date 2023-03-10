9929a4538b08bb40636d9083a12b767631ec844e
markazali <mjazali@gmail.com>
2023-03-10 14:47:58 -0800

    init

---
+++ SQLFileLoader/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace SQLFileLoader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBoxQuery = new System.Windows.Forms.TextBox();
+            this.textBoxSearch = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(36, 47);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(736, 21);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // textBoxQuery
+            // 
+            this.textBoxQuery.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxQuery.Location = new System.Drawing.Point(36, 74);
+            this.textBoxQuery.Multiline = true;
+            this.textBoxQuery.Name = "textBoxQuery";
+            this.textBoxQuery.Size = new System.Drawing.Size(736, 346);
+            this.textBoxQuery.TabIndex = 1;
+            // 
+            // textBoxSearch
+            // 
+            this.textBoxSearch.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxSearch.ForeColor = System.Drawing.SystemColors.InactiveCaption;
+            this.textBoxSearch.Location = new System.Drawing.Point(36, 21);
+            this.textBoxSearch.Name = "textBoxSearch";
+            this.textBoxSearch.Size = new System.Drawing.Size(736, 20);
+            this.textBoxSearch.TabIndex = 2;
+            this.textBoxSearch.Text = "Search";
+            this.textBoxSearch.TextChanged += new System.EventHandler(this.textBoxSearch_TextChanged);
+            this.textBoxSearch.Enter += new System.EventHandler(this.textSearch_Enter);
+            this.textBoxSearch.Leave += new System.EventHandler(this.textBoxSearch_Leave);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBoxSearch);
+            this.Controls.Add(this.textBoxQuery);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "InvoTech Script Tool";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBoxQuery;
+        private System.Windows.Forms.TextBox textBoxSearch;
+    }
+}
+
---
+++ SQLFileLoader/Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SQLFileLoader
+{
+    public partial class Form1 : Form
+    {
+        IDictionary<string, string> scriptDict = new Dictionary<string, string>();
+        String currentDir = AppDomain.CurrentDomain.BaseDirectory;
+        public Form1()
+        {
+            InitializeComponent();
+
+
+
+
+
+        }
+        private void textSearch_Enter(object sender, EventArgs e)
+        {
+            if (textBoxSearch.Text == "Search")
+            {
+                textBoxSearch.Text = "";
+                textBoxSearch.ForeColor = Color.Black;
+            }
+            else
+            {
+
+            }
+        }
+
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                String test = comboBox1.Text;
+                textBoxQuery.Text = scriptDict[test];
+            }
+            catch
+            {
+
+            }
+
+        }
+
+
+
+        private void textBoxSearch_TextChanged(object sender, EventArgs e)
+        {
+            comboBox1.Items.Clear();
+
+            comboBox1.ResetText();
+
+            if (textBoxSearch.Text == "")
+            {
+                return;
+            }
+
+            for (int index = 0; index < scriptDict.Count; index++)
+            {
+                var item = scriptDict.ElementAt(index);
+                var itemKey = item.Key;
+                var itemValue = item.Value;
+
+                if (itemValue.ToLower().Contains(textBoxSearch.Text.ToLower()))
+                {
+                    comboBox1.Items.Add(itemKey);
+                }
+
+            }
+
+
+        }
+
+        private void textBoxSearch_Leave(object sender, EventArgs e)
+        {
+            if (textBoxSearch.Text == "")
+            {
+                textBoxSearch.Text = "Search";
+                textBoxSearch.ForeColor = Color.Silver;
+                FillOptions();
+            }
+        }
+
+        private void FillOptions()
+        {
+            string[] filePaths = Directory.GetFiles(AppDomain.CurrentDomain.BaseDirectory);
+
+            foreach (String file in filePaths)
+            {
+                if (file.Contains(".sql") )
+                {
+                    if (!scriptDict.ContainsKey(Path.GetFileName(file))) {
+                        scriptDict.Add(Path.GetFileName(file), File.ReadAllText(file));
+                        
+                    }
+                    comboBox1.Items.Add(Path.GetFileName(file));
+
+                }
+
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            FillOptions();
+        }
+    }
+}
